2611b4083c36f73a3975a80ceb14cc621be141cf
HP <HP@LAPTOP-8NCV6PNI>
2023-10-04 15:50:59 +0100

    Trial Testing

---
+++ src/com/obsqura/program/P2/Details.java
@@ -0,0 +1,15 @@
+package com.obsqura.program.P2;
+
+public class Details {
+
+	public static void main(String[] args) {
+		String name = "Amala";
+		int age = 28;
+		
+	    System.out.println("Name is " + name);
+	  
+	    System.out.println("Age is "+ age);
+
+	}
+
+}
---
+++ src/com/obsqura/program/P2/Test1.java
@@ -0,0 +1,13 @@
+package com.obsqura.program.P2;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		int a = 25;
+		int b = 4;
+		int c = a+b;
+		System.out.println(c);
+
+	}
+
+}
