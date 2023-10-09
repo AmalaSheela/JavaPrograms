86102fb9e77e04734b3b081ba4100fd1b2451fa7
HP <HP@LAPTOP-8NCV6PNI>
2023-10-09 19:10:18 +0100

    string program reference

---
+++ src/com/obsqura/program/P2/Substring.java
@@ -0,0 +1,13 @@
+package com.obsqura.program.P2;
+
+public class Substring {
+
+	public static void main(String[] args) 
+	{
+		String s1="HelloWorld";  
+		System.out.println(s1.substring(5,10));
+		
+		}
+	}  
+
+
