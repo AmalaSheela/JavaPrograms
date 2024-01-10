52731561272d94ac899b975f7853dbd27e84fb01
HP <HP@LAPTOP-8NCV6PNI>
2024-01-10 19:50:38 +0000

    Staticaccount

---
+++ src/com/obsqura/pgm/StaticMethod/Staticaccount.java
@@ -0,0 +1,41 @@
+package com.obsqura.pgm.StaticMethod;
+
+public class Staticaccount {
+	
+	//Method1 currentbalance
+	public static int currentbalance(int balance)
+	{
+		return balance;
+		
+	}
+
+	//Method2
+	public static int deposit(int amount, int currentBalance)
+	{
+	 currentBalance =  currentBalance + amount;
+	 return currentBalance;
+	}
+		
+
+	//Method3
+	public static int withDraw(int amount,int currentbalance)
+	{
+	currentbalance=currentbalance-amount;
+	return currentbalance;
+	}
+
+	//Main method
+	public static void main(String args[])
+	{
+	int amount=1000;
+	int bal = Staticaccount.currentbalance(amount);
+	System.out.println("Balance is " + bal);
+	int bal1 = Staticaccount.deposit(250, bal);
+	System.out.println("Balance is " + bal1);
+	int bal2 = Staticaccount.withDraw(350, bal);
+	System.out.println("Balance is " + bal2);
+
+	}
+
+
+}
